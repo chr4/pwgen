25f5e64b04069ca0c057c3967d2ca054a719133d
Chris Aumann <me@chr4.org>
2017-03-30 22:41:38 +0200

    Remove () from comments of exported functions

--- pwgen.go
+++ pwgen.go
@@ -9,7 +9,7 @@ var num = "0123456789"
 var alphaNum = num + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 var alphaNumSymbols = alphaNum + "_-?!.,@#$%^&*()=[]{}<>"
 
-// New() generates a random string of the given length out of the characters in char
+// New generates a random string of the given length out of the characters in char
 func New(length int, chars string) string {
 	var bytes = make([]byte, length)
 	var op = byte(len(chars))
@@ -21,18 +21,18 @@ func New(length int, chars string) string {
 	return string(bytes)
 }
 
-// Num() generates a random string of the given length out of numeric characters
+// Num generates a random string of the given length out of numeric characters
 func Num(length int) string {
 	return New(length, num)
 }
 
-// AlphaNum() generates a random string of the given length out of alphanumeric characters
+// AlphaNum generates a random string of the given length out of alphanumeric characters
 func AlphaNum(length int) string {
 	return New(length, alphaNum)
 }
 
-// AlphaNumSymbols() generates a random string of the given length out of alphanumeric characters
-// and symbols
+// AlphaNumSymbols generates a random string of the given length out of alphanumeric characters and
+// symbols
 func AlphaNumSymbols(length int) string {
 	return New(length, alphaNumSymbols)
 }
